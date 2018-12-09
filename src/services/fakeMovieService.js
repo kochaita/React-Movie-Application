e108b3db6aca7c07020eb37ebe2a9a70379689e5
Ayon Das <ayond95@gmail.com>
2018-12-09 18:58:03 +0530

    Updated Service

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -1,5 +1,3 @@
-import * as genresAPI from "./fakeGenreService";
-
 const movies = [
   {
     _id: "5b21ca3eeb7f6fbccd471815",
@@ -69,29 +67,4 @@ const movies = [
 
 export function getMovies() {
   return movies;
-}
-
-export function getMovie(id) {
-  return movies.find(m => m._id === id);
-}
-
-export function saveMovie(movie) {
-  let movieInDb = movies.find(m => m._id === movie._id) || {};
-  movieInDb.name = movie.name;
-  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-  movieInDb.numberInStock = movie.numberInStock;
-  movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-  if (!movieInDb._id) {
-    movieInDb._id = Date.now();
-    movies.push(movieInDb);
-  }
-
-  return movieInDb;
-}
-
-export function deleteMovie(id) {
-  let movieInDb = movies.find(m => m._id === id);
-  movies.splice(movies.indexOf(movieInDb), 1);
-  return movieInDb;
 }
